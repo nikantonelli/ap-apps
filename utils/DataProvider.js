b9b5e2a30b432a53acf53978b80c2892a905678d
nikantonelli <nik@nikantonelli>
2023-05-14 21:28:48 +0100

    dev work

--- utils/DataProvider.js
+++ utils/DataProvider.js
@@ -2,6 +2,6 @@ import AgilePlace from "./AgilePlace"
 
 export const DataProvider = {
 	get: function() {
-		return new AgilePlace(process.env.AGILEPLACE, process.env.AGILEUSER, process.env.AGILEPASS, process.env.AGILEKEY)
+		return new AgilePlace()
 	}
 }
