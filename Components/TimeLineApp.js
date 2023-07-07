b91c483c0efa707d32dd7f7a0fc259bae1527447
nikantonelli <nikantonelli@users.noreply.github.com>
2023-07-07 14:26:54 +0100

    Fiddling with timeline

--- Components/TimeLineApp.js
+++ Components/TimeLineApp.js
@@ -14,62 +14,82 @@ export class TimeLineApp extends React.Component {
 			var nodes = this.props.data
 
 
-			var dateToSize = scaleLinear()
+			var dateToPosn = scaleLinear()
 				.domain([this.props.start, this.props.end])
-				.range([0, 100])
+				.range([0, 100.0])
 
-				var sizeToDate = scaleLinear()
-				.domain([0, 100])
+			var posnToDate = scaleLinear()
+				.domain([0, 100.0])
 				.range([this.props.start, this.props.end])
 
 			var barHeight = "30px";
 
 			var dateTicks = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90];
 
-			var tlg = <Box key={0} sx={{ width: "100%" }}>
-				<Grid container direction='row'>
-					<Grid sx={{ width: "20%", maxWidth: 400 }}>
-						<Grid container direction="row">
-						<Box sx={{ height: barHeight }}>
-								<Typography sx={{ textAlign: "left", cursor: 'pointer' }}>
-									Item Title
-								</Typography>
-						</Box>
-						<Box sx={{ height: barHeight }}>
-								<Typography sx={{ textAlign: "right", cursor: 'pointer' }}>
-									Date:
-								</Typography>
-						</Box>
+			var tlg =
+				<Box key={0} sx={{ width: "95%" }}>
+					<Grid container direction='row'>
+						<Grid sx={{ width: "20%", maxWidth: 400 }}>
+							
+						</Grid>
+						<Grid sx={{ margin: "2px 0px 0px 0px", width: "80%" }}>
+							<Grid id="timelineRow" container direction='row'>
+								{dateTicks.map((width, idx) => {
+									return <Grid key={idx} sx={{ width: "10%" }}>
+										<Box sx={{ height: barHeight, width: '100%' }}>
+											<Typography sx={{ fontSize: "8px" }}>
+												{new Date(posnToDate(width)).toLocaleDateString("en-GB")}
+											</Typography>
+											<Box sx={{ height: "50%", width: "1px", backgroundColor: "black" }} />
+										</Box>
+									</Grid>
+								})
+								}
+							</Grid>
 						</Grid>
 					</Grid>
-					<Grid sx={{ margin: "2px 0px 0px 0px", width: "80%" }}>
-						<Grid id="timelineRow" container direction='row'>
-							{dateTicks.map((width, idx) => {
-								return <Grid sx={{ width: "10%" }}>
-									<Box sx={{ height: barHeight, width: '100%'}}>
-										<Typography sx={{fontSize:"8px"}}>
-											{new Date(sizeToDate(width)).toDateString()}
-										</Typography>
-										<Box sx={{ height:"50%", width: "1px", backgroundColor: "black"}} />
-									</Box>
-								</Grid>
-							})
-							}
-						</Grid>
-					</Grid>
-				</Grid>
-			</Box>
+				</Box>
 
 
 			var blg = nodes.map((node, idx) => {
-				var startPercent = node.data.plannedStart ? dateToSize(new Date(node.data.plannedStart).getTime()) : 0
-				var endPercent = node.data.plannedFinish ? dateToSize(new Date(node.data.plannedFinish).getTime()) : 0
+				//dateToPosn returns undefined if date is outside the range
+				var plannedStartPC = node.data.plannedStart ? dateToPosn(new Date(node.data.plannedStart).getTime()) : undefined
+				var plannedEndPC = node.data.plannedFinish ? dateToPosn(new Date(node.data.plannedFinish).getTime()) : undefined
+
+				var actualStartPC = node.data.actualStart ? dateToPosn(new Date(node.data.actualStart).getTime()) : undefined
+				var actualEndPC = node.data.actualFinish ? dateToPosn(new Date(node.data.actualFinish).getTime()) : undefined
+
+				var nowPosn = dateToPosn(Date.now())
+
+				var colourBar = "#27a444"
+				if (plannedStartPC !== undefined) {
+
+					if (plannedStartPC < 0) plannedStartPC = 0;
+					if (plannedStartPC > 100) plannedStartPC = 100;
+				}
+				if (plannedEndPC !== undefined) {
+
+					if (plannedEndPC < 0) plannedEndPC = 0;
+					if (plannedEndPC > 100) plannedEndPC = 100;
+				}
+				if (actualStartPC !== undefined) {
+
+					if (actualStartPC < 0) actualStartPC = 0;
+					if (actualStartPC > 100) actualStartPC = 100;
+				}
+				if (actualEndPC !== undefined) {
+					colourBar = "#444444";
+					if (actualEndPC < 0) actualEndPC = 0;
+					if (actualEndPC > 100) actualEndPC = 100;
+				} else {
+					actualEndPC = nowPosn;
+				}
 
 				return (
-					<Box key={idx+1} sx={{ width: "100%" }}>
+					<Box key={idx + 1} sx={{ width: "95%" }}>
 						<Grid container direction='row'>
-							<Grid sx={{ width: "20%", maxWidth: 400 }}>
-								<Box sx={{ height: barHeight }}>
+							<Grid sx={{ borderTop: "3px double #aaaaaa", width: "20%", maxWidth: 400, height: barHeight }}>
+								<Box >
 									<Tooltip title={node.data.title}>
 										<Typography key={idx} variant="body2" className="timeline-text" sx={{ textAlign: "center", cursor: 'pointer' }} onClick={this.props.onClick}>
 											{node.data.title}
@@ -77,18 +97,42 @@ export class TimeLineApp extends React.Component {
 									</Tooltip>
 								</Box>
 							</Grid>
-							<Grid sx={{ margin: "2px 0px 0px 0px", width: "80%" }}>
-								<Grid id="timelineRow" container direction='row'>
-									<Grid sx={{ width: startPercent.toFixed(1).toString() + "%" }}>
-										<Box sx={{ zIndex: 999, borderRadius: "5px", height: barHeight, width: '100%', backgroundColor: 'lightgrey' }} />
+							<Grid sx={{ borderTop: "3px double #aaaaaa", width: "80%", height: barHeight }}>
+								{((plannedStartPC !== undefined) && (plannedEndPC !== undefined)) ?
+									<Grid id="timelineRow" container direction='row'>
+										<Grid sx={{ width: plannedStartPC.toString() + "%" }}>
+											<Box sx={{ height: "100%", backgroundColor: 'lightgrey' }} />
+										</Grid>
+										<Grid sx={{ width: (plannedEndPC - plannedStartPC).toString() + "%" }}>
+											<Box sx={{ borderBottom: "solid 4px #4989e4", height: "8px", backgroundColor: this.props.colourise(node) }} />
+										</Grid>
+										<Grid sx={{ width: (100.0 - plannedEndPC).toString() + "%" }}>
+											<Box sx={{ height: "100%", backgroundColor: 'lightgrey' }} />
+										</Grid>
 									</Grid>
-									<Grid sx={{ width: (endPercent - startPercent).toFixed(1).toString() + "%" }}>
-										<Box sx={{ margin: "0px 1px 0px 0px", border: "solid 1px black", borderRadius: "5px", height: barHeight, width: '100%', backgroundColor: this.props.colourise(node) }} />
+									:
+
+									<Grid sx={{ width: "100%" }}>
+										<Box sx={{ height: "12px", backgroundColor: 'lightgrey' }} />
 									</Grid>
-									<Grid sx={{ width: (100.0 - endPercent).toFixed(1).toString() + "%" }}>
-										<Box sx={{ zIndex: 999, borderRadius: "5px", height: barHeight, width: '100%', backgroundColor: 'lightgrey' }} />
+								}
+
+								{((actualEndPC !== undefined) && (actualStartPC !== undefined)) ?
+									<Grid id="timelineRow" container direction='row'>
+										<Grid sx={{ width: actualStartPC.toString() + "%" }}>
+											<Box sx={{ height: "100%", backgroundColor: 'lightgrey' }} />
+										</Grid>
+										<Grid sx={{ width: (actualEndPC - actualStartPC).toString() + "%" }}>
+											<Box sx={{ borderBottom: "solid 4px " + colourBar, height: "8px", backgroundColor: this.props.colourise(node) }} />
+										</Grid>
+										<Grid sx={{ width: (100.0 - actualEndPC).toString() + "%" }}>
+											<Box sx={{ height: "100%", backgroundColor: 'lightgrey' }} />
+										</Grid>
 									</Grid>
-								</Grid>
+									:
+									<Grid sx={{ width: "100%" }}>
+										<Box sx={{ height: "12px", backgroundColor: 'lightgrey' }} />
+									</Grid>}
 							</Grid>
 						</Grid>
 					</Box>
