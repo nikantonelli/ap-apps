276f0c42726cd321fdafa97c1b3973da8156bfc4
nikantonelli <nikantonelli@users.noreply.github.com>
2023-09-15 16:42:35 +0100

    Add a video of what it does so far

--- Components/ReqsProgress.js
+++ Components/ReqsProgress.js
@@ -1,22 +1,23 @@
-import { Grid, LinearProgress, Typography } from "@mui/material";
+import { Grid, LinearProgress, Paper, Typography } from "@mui/material";
 
-export function ReqsProgress({pending, complete}) {
-    return (
-        <Grid container direction='column' sx={{ display: 'flex', alignItems: 'center' }}>
-					<Grid>
-						<Typography variant="h6">Loading, please wait</Typography>
-					</Grid>
-					<Grid sx={{ width: '100%' }}>
-						<Grid container direction="row">
-							<Grid xs={10} item>
-								<LinearProgress variant="determinate" value={Math.round((complete) / (pending ? pending : 1) * 100)} />
-							</Grid>
-							<Grid xs={2} item>
-								<Typography variant="body2" color="text.secondary">{pending}</Typography>
-							</Grid>
+export function ReqsProgress({ pending, complete }) {
+	return (
+		<Paper elevation={4} sx={{margin:"3px"}}>
+			<Grid container direction='column' sx={{ margin:"3px", display: 'flex', alignItems: 'center' }}>
+				<Grid>
+					<Typography variant="body">Loading, please wait</Typography>
+				</Grid>
+				<Grid sx={{ width: '100%' }}>
+					<Grid container direction="row">
+						<Grid xs={10} item>
+							<LinearProgress variant="determinate" value={Math.round((complete) / (pending ? pending : 1) * 100)} />
+						</Grid>
+						<Grid xs={2} item>
+							<Typography variant="body2" color="text.secondary">{pending}</Typography>
 						</Grid>
 					</Grid>
-
 				</Grid>
-    )
+			</Grid>
+		</Paper>
+	)
 }
